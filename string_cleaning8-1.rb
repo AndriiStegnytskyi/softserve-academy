f9c599962f514a4992cc34c36948d43ed2fd37fb
AndriiStegnytskyi <a.stegnytskyi@gmail.com>
2022-06-24 12:42:56 +0200

    Update string_cleaning8-1.rb

--- string_cleaning8-1.rb
+++ string_cleaning8-1.rb
@@ -1,3 +1,4 @@
-puts "Please enter the string, which is to be corrected"
-string_to_be_corrected=gets.chomp
-puts "Your new string is:  "+string_to_be_corrected.gsub(/\d+/, '')
+def string_clean(string)
+  return string.gsub(/\d+/, '')
+
+end
