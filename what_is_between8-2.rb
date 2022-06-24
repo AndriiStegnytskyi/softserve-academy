f0f60c2388d1684ddb198a84713e7ee617887654
AndriiStegnytskyi <a.stegnytskyi@gmail.com>
2022-06-24 12:48:33 +0200

    Update what_is_between8-2.rb

--- what_is_between8-2.rb
+++ what_is_between8-2.rb
@@ -1,16 +1,7 @@
-def return_integers_range (a,b)
-  puts "\n"+"Your numbers are:"
-  (a..b).each do |digit|
-    puts "\n"+digit.to_s
+def between(a, b)
+  init_arr = []
+(a..b).each do |digit|
+  init_arr << digit
   end
-end
-
-puts "Enter first number"
-a = gets.chomp.to_i
-puts "Enter second number (must be greater than the first one)"
-b = gets.chomp.to_i
-if (a>b)
-  puts "Your first number is smaller than the second one"
-else
-return_integers_range(a,b)
+  return init_arr
 end
